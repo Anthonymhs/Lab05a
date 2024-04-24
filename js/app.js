b3e51a743d999e0d994d84fa3defb6c955bb7ec1
ANTHONY <anthonymhs0320@gmail.com>
2024-04-23 21:10:40 -0500

    problem3 realizado

--- js/app.js
+++ js/app.js
@@ -14,6 +14,7 @@ function sum(a, b) { //eslint-disable-line
     return [suma,message];
 }
 sum(4 , 7);
+
 // Here is the test for sum(); uncomment it to run it
 testSum(4, 7);
 
@@ -47,16 +48,23 @@ Third element: "4 and 7 and 5 sum to 16."
 Fourth element: "The product of 4 and 7 and 5 is 140."
 
 IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this function. To do addition, use your sum() function, and to do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this. However, you may continue to use the + operator for string concatenation.
-
+4 and 7 and 5 sum to 16.
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+    const sumAB = sum(a, b);
+    const sumABC = sum(sumAB[0], c);
+    const multiplyAB = multiply(a, b);
+    const multiplyABC = multiply(multiplyAB[0], c);
+    const sumMessage = a + ' and ' + b + ' and ' + c + ' sum to ' + sumABC[0] + '.';
+    const multiplyMessage = 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + multiplyABC[0] + '.';
+    return [sumABC[0], multiplyABC[0], sumMessage, multiplyMessage];
 }
+sumAndMultiply(4 , 7 , 5);
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
