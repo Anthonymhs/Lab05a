c3a9a1815d2f18c23541f2830c20e35f25e357e0
ANTHONY <anthonymhs0320@gmail.com>
2024-04-23 22:06:01 -0500

    realizado problem 4

--- js/app.js
+++ js/app.js
@@ -96,7 +96,7 @@ sumArray([2, 3, 4]);
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
