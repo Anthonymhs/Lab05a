a2a4c07530a008ad9e999a860eeb784b54805e0d
ANTHONY <anthonymhs0320@gmail.com>
2024-04-23 20:32:44 -0500

    resolved problem2

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
     const suma = a + b;
-    const message= "The sum of " + a + " and " + b + " is " + suma + ".";
+    const message= 'The sum of ' + a + ' and ' + b + ' is ' + suma + '.';
     return [suma,message];
 }
 sum(4 , 7);
@@ -29,11 +29,13 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-
+    const multiplication = a * b;
+    const message= 'The product of ' + a + ' and ' + b + ' is ' + multiplication + '.';
+    return [multiplication,message];
 }
-
+multiply(5 , 9);
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
